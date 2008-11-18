7bcb5c29f8f7f13582b6d86b2a9f75537c50b476
Daniel Johnson <ComputerDruid@gmail.com>
2008-11-17 20:43:30 -0500

    speed bumpers can now be passthrough

--- SpeedBumper.java
+++ SpeedBumper.java
@@ -6,6 +6,11 @@
          super(x,y,width,height);
 			xVel = xV;
 			yVel = yV;
+      }
+        public SpeedBumper(double x, double y, double width, double height, double xV, double yV, boolean pass){
+         super(x,y,width,height,pass);
+			xVel = xV;
+			yVel = yV;
       }            
        public double getXVelocity()
       {
