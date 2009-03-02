46996aaac988b2d100b7fe4843c6979263f1f018
Daniel Johnson <ComputerDruid@gmail.com>
2009-03-02 17:59:30 -0500

    added finishing stuffs in prep for switchover

--- Display.cpp
+++ Display.cpp
@@ -5,3 +5,6 @@ void blit(int x, int y, SDL_Surface* source, SDL_Surface* dest){
 	offset.y=y;
 	SDL_BlitSurface(source, NULL, dest, &offset );
 }
+SDL_Surface* create_rectangle(int width, int height){
+	//SDL_Surface* temp = 
+}
